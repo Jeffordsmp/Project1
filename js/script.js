d646a8651fc899180be43fc898fcac6e38ec2724
Choop <matthewjeffords97@yahoo.com>
2020-01-06 20:26:11 -0700

    adding a second ajax call

--- js/script.js
+++ js/script.js
@@ -1,19 +1,39 @@
-APIKeys = ["97ed52ef559840adbd93fed9102d1c8e", "909ff812fb1445139e4775853f4d6a4a", "362fac0282c242aabd0952d77fc3a515"]
+var APIKeys = ["97ed52ef559840adbd93fed9102d1c8e", "909ff812fb1445139e4775853f4d6a4a", "362fac0282c242aabd0952d77fc3a515"]
 
 function getRecipe() {
 
-    APIKey = APIKeys[Math.floor(Math.random() * 3)]
+    var APIKey = APIKeys[Math.floor(Math.random() * 3)]
 
     console.log(APIKey)
 
-    ingredients = "apples,+flour,+sugar"
+    var ingredients = "apples,+flour,+sugar"
 
-    var queryURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=2&apiKey=${APIKey}`
+    var queryURL = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&number=1&apiKey=${APIKey}`
 
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
         console.log(response)
+        seeRecipe(response.id)
+    })
+}
+
+function seeRecipe(x) {
+
+    var APIKey = APIKeys[Math.floor(Math.random() * 3)]
+
+    console.log(APIKey)
+
+    var id = x
+
+    var queryURL = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${APIKey}`
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response)
+        
     })
 }
